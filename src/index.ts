b4e5f104e00e27d2c93ab146664ff7e2378c94f7
Nitesh Balusu <niteshbalusu@icloud.com>
2023-03-20 01:24:30 -0400

    switch sample event creation to a separate file
    Signed-off-by: Nitesh Balusu <niteshbalusu@icloud.com>

--- src/index.ts
+++ src/index.ts
@@ -14,27 +14,6 @@ const main = async () => {
   return await auto({
     startServer: [
       async () => {
-        const sampleJson = {
-          id: '123',
-          pubkey: '55a3fd367611e5161719388e9ca8d0eea5fea62374340bf98c85c3c0fcd42a57',
-          created_at: Math.round(Date.now() / 1000),
-          kind: 700,
-          tags: ['test', 'test2'],
-          content: 'test content 234',
-          sig: '123',
-        };
-
-        const event = ['EVENT', sampleJson];
-        const privateKey = '35f9a7cdda90ded86eb15279e0b1f51358c399b6dd08075eb7469f9b67454eab';
-
-        const hash: any = crypto.createHash('sha256').update(JSON.stringify(event)).digest();
-
-        sampleJson.id = unit8AsHex(hash);
-
-        sampleJson.sig = unit8AsHex(tinysecp.signSchnorr(hash, hexAsBuffer(privateKey)));
-
-        console.log(JSON.stringify(event));
-
         startWebsocket();
       },
     ],
